4c60ecf07eb20c78a5905acdd83306f4752af5a0
Lucas Mendelowski <mendelowski@gmail.com>
2017-12-19 21:59:45 +0100

    Bump neovim version

--- Formula/neovim.rb
+++ Formula/neovim.rb
@@ -1,9 +1,9 @@
 class Neovim < Formula
   desc "Ambitious Vim-fork focused on extensibility and agility"
   homepage "https://neovim.io"
-  url "https://github.com/neovim/neovim/releases/download/v0.2.1/nvim-macos.tar.gz"
-  version "0.2.1"
-  sha256 "8ddafa48f9ed16cd97cb00ab94adcf451e0ffd498799f163e1935a89f14ff8f7"
+  url "https://github.com/neovim/neovim/releases/download/v0.2.2/nvim-macos.tar.gz"
+  version "0.2.2"
+  sha256 "18c3d048d8670515be9255d8b4a3c3b9f869f0cf4a40538fe7b85214edae44b3"
 
   def install
     chmod 0755, "bin/nvim"
